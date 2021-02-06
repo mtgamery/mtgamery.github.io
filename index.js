54e4e8249ba4df25771a18ed91b8285d41e9f95f
mtgamery <mtgamery@gmail.com>
2021-02-06 11:53:57 -0800

    index update

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ $(function() {
 		window.open("https://www.youtube.com/channel/UCmydpynciKTM5SObIzbMKFg");
 	});
 
-	$("#chapter1").click(function(){
-		window.location.href = "codecases/chapter1/intro.html";
+	$("#investigation").click(function(){
+		window.open("https://forms.gle/BG3RkWhKw9zYRS1y5");
+	});
+
+	$("#cc_chapter1").click(function(){
+		window.open("codecases/chapter1/intro.html");
 	});
 });
 
 function loadContent(){
 	$("#title").attr("src","https://docs.google.com/uc?export=download&id=1w4TLBFwztL3n4hRrI6jAVDq4hbquu17F");
 	$("#youtube").attr("src","https://docs.google.com/uc?export=download&id=1EHIzdtEp_5yOrw4aZL_DS3H0mYvRCgC0");
-	$("#chapter1").attr("src","https://docs.google.com/uc?export=download&id=1FX_1o_kPeLiBL09kBRA18nuoxiVtTHC4");
+	$("#investigation").attr("src","https://docs.google.com/uc?export=download&id=1o7CNLDqOgzyvjXZGfRR2aLbH2V6uAiqI");	
+	$("#cc_chapter1").attr("src","https://docs.google.com/uc?export=download&id=12r0yr0xorGZvdDkGBs4CMIS160-3LLzU");
 }
